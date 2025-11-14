5384c545c84ec942757e7810c7cf7b53e2a27921
AustinArvelo <austina46@nycstudents.net>
2025-11-14 13:19:21 -0500

    Started writing read methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) throws FileNotFoundException {
-        System.out.println();
+        System.out.println(read());
     }
-    public static
+    public static String read() throws FileNotFoundException {
+        File f = new File("Scoreboard.txt");
+        Scanner s = new Scanner(f);
+        String teams[] = new String[12];
+        while (s.hasNextLine()){
+            String team1 = s.next();
+            int points1 = 0;
+            String team2 = s.next();
+            int points2 = 0;
+            for (int i = 0; i < teams.length; i++){
+                if (team1.equals(teams[i])) points1 += teams[i[]]
+            }
+            if (s.next == 0) sw
+        }
+    }
 }
